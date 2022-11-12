2d81214f6324760d27da091e034dc407cd83c8f8
Novaisn <nelsonnovais@ipvc.pt>
2022-11-12 11:42:25 +0000

    first commit

---
+++ Scripts/TipoPagamento.sql
@@ -0,0 +1,5 @@
+insert into TipoPagamento (Descricao) values ('china-unionpay');
+insert into TipoPagamento (Descricao) values ('diners-club-us-ca');
+insert into TipoPagamento (Descricao) values ('solo');
+insert into TipoPagamento (Descricao) values ('jcb');
+insert into TipoPagamento (Descricao) values ('switch');
